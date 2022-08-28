2b7e3bfdad4dbe9d5179c17bd12885788a46d7b6
AlexanderGonchar <goncharas24@gmail.com>
2022-08-28 07:27:39 +0300

    add z1

---
+++ z1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число обозначающее день недели, что бы выяснить, является ли номер дня недели выходным: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число обозначающее день недели: " + number);
+if (number > 0 && number <= 7)
+{
+    if (number < 6)
+    {
+        Console.WriteLine("Номер дня недели НЕ является выходным");
+    }
+    else if (number >= 6)
+    {
+        Console.WriteLine("Номер дня недели Является выходным");
+    }
+}
+else
+    Console.WriteLine("В неделе семь дней! Введите число от 1 до 7");
